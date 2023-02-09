955924c2073bc0a977d12b2f996a31fc09acefa6
AminaChakirDev <amina.aitm@gmail.com>
2023-02-09 09:45:53 +0100

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'soutien_angular_routing';
+  title = 'lc_deploy_angular';
 }
